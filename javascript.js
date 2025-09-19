97783401f71fa008a59957a2dce88bee16ef0780
Jack Bawer <jackb2507@gmail.com>
2025-09-19 22:27:52 +0100

    UI in shambles

--- javascript.js
+++ javascript.js
@@ -1,150 +1,70 @@
+const buttons = document.querySelectorAll('input');
+let result = document.querySelector("#result");
+
 // Random choice (rock, paper, scissors)
 function getComputerChoice() {
-  computerChoice = Math.floor(Math.random() * 3);
-  switch (computerChoice) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// Get user's choice
-function getUserChoice() {
-  while (true) {
-    let userChoice = prompt("Rock🗿, paper📜, or scissors✂!");
-    userChoice = userChoice.toLowerCase();
-    if (
-      userChoice === "rock" ||
-      userChoice === "paper" ||
-      userChoice === "scissors"
-    ) {
-      return userChoice;
-    }
-    console.log("Enter rock🗿, paper📜, or scissors✂, please!");
-  }
+// Score function
+function getScore(userScore, computerScore) {
+    const divScore = document.querySelector(".divScore");
+    const score = document.createElement("p");
+    score.innerText = `Hughmann ${userScore} - ${computerScore} Machine `;
+    divScore.appendChild(score);
 }
 
-// play game
-
-function playGame() {
-  let userScore = 0;
-  let computerScore = 0;
-
-  // Play round
-  function playRound(getUserChoice, getComputerChoice) {
-    user = getUserChoice;
-    computer = getComputerChoice;
-
-    if (user === "rock") {
-      if (computer === "rock") {
-        console.log(
-          "Draw!\nThe score is: " + userScore + " : " + computerScore,
-        );
-        return;
-      }
-      if (computer === "paper") {
-        computerScore++;
-        console.log(
-          "You lose! Paper📜 beats Rock🗿\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
-      if (computer === "scissors") {
+// Play round function
+function playRound(user, computer, userScore, computerScore) {
+    if (user === computer) {
+        // It's a tie
+        getScore(userScore, computerScore);
+    } else if (user === "rock" && computer === "scissors" 
+        || user === "paper" && computer === "rock"
+        || user === "scissors" && computer === "paper") {
+        // User wins
         userScore++;
-        console.log(
-          "You win! Rock🗿 beats scissors✂\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
-    } else if (user === "paper") {
-      if (computer === "paper") {
-        console.log(
-          "Draw!\nThe score is: " + userScore + " : " + computerScore,
-        );
-        return;
-      }
-      if (computer === "rock") {
-        userScore++;
-        console.log(
-          "You win! Paper📜 beats Rock🗿\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
-      if (computer === "scissors") {
-        computerScore++;
-        console.log(
-          "You lose! Scissors✂ beats Paper📜\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
+        getScore(userScore, computerScore);
     } else {
-      if (computer === "scissors") {
-        console.log(
-          "Draw!\nThe score is: " + userScore + " : " + computerScore,
-        );
-        return;
-      }
-      if (computer === "rock") {
+        // Computer wins
         computerScore++;
-        console.log(
-          "You lose! Rock🗿 beats Scissors✂\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
-      if (computer === "paper") {
-        userScore++;
-        console.log(
-          "You win! Scissors✂ beats Paper📜\nThe score is: " +
-            userScore +
-            " : " +
-            computerScore,
-        );
-        return;
-      }
+        getScore(userScore, computerScore);
     }
-  }
-  const numRounds = 5;
 
-  while (userScore <= numRounds || computerScore <= numRounds) {
-    const userSelection = getUserChoice();
-    const computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
-    if (userScore === numRounds) {
-      console.log(
-        "You win the game!\nThe score is: " + userScore + " : " + computerScore,
-      );
-      return;
-    }
-    if (computerScore === numRounds) {
-      console.log(
-        "You lose the game...\nThe score is: " +
-          userScore +
-          " : " +
-          computerScore,
-      );
-      return;
-    }
-  }
+    // Return updated scores
+    return { userScore, computerScore };
+}
+
+// Play game function
+function playGame() {
+    let result = "";
+    let userScore = 0;
+    let computerScore = 0;
+    const numRounds = 5;
+
+    // Add event listeners once
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Get the user's choice and computer's choice
+            const userChoice = button.value;
+            const computerChoice = getComputerChoice();
+
+            // Play the round and get updated scores
+            const scores = playRound(userChoice, computerChoice, userScore, computerScore);
+            userScore = scores.userScore;
+            computerScore = scores.computerScore;
+
+            // Check for a winner
+            if (userScore === numRounds) {
+                result = "You win!";
+            } else if (computerScore === numRounds) {
+                result = "You lose!";
+            }
+        });
+    });
 }
 
 // Main function
-
 playGame();
+
